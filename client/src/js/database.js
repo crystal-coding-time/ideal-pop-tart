71f50a48f40128b93605d56b2a68a1842bcd4e4e
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2023-03-31 15:45:46 -0400

    Added putDb and getDb methods

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,34 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Define a function to add content to the 'jate' database
+export const putDb = async (content) => {
+  // Initialize the 'jate' database
+  const db = await initdb();
+  // Create a transaction in 'readwrite' mode
+  const tx = db.transaction('jate', 'readwrite');
+  // Retrieve the 'jate' object store from the transaction
+  const store = tx.objectStore('jate');
+  // Add the content to the 'jate' object store using the 'put' method
+  await store.put(content);
+  // Wait for the transaction to complete
+  await tx.done;
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
-initdb();
+// Define a function to get all the content from the 'jate' database
+export const getDb = async () => {
+  // Initialize the 'jate' database
+  const db = await initdb();
+  // Create a transaction in 'readonly' mode
+  const tx = db.transaction('jate', 'readonly');
+  // Retrieve the 'jate' object store from the transaction
+  const store = tx.objectStore('jate');
+  // Get all the content from the 'jate' object store using the 'getAll' method
+  const result = await store.getAll();
+  // Wait for the transaction to complete
+  await tx.done;
+  // Return the result of the 'getAll' method
+  return result;
+};
+  
+  initdb();
