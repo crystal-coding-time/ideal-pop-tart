8f7bb5d01d5c4af49a2dd2a4ae4d6e5db708a5a2
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2023-04-01 13:42:47 -0400

    debugging code

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,10 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+
+// Create a new cache for assets
+const assetCache = new CacheFirst({
+  cacheName: 'asset-cache',
+});
+
 registerRoute();
