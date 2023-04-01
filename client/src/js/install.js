93d1517b2bc4a4d2d0134c821107d71d9aae94d2
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2023-04-01 12:33:00 -0400

    added to install file

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,36 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let deferredPrompt;
+
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the default behavior
+  event.preventDefault();
+  // Save the prompt event for later use
+  deferredPrompt = event;
+  // Show the install button
+  butInstall.style.display = 'block';
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+  // If there is a deferred prompt, show it
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const choiceResult = await deferredPrompt.userChoice;
+    // Log the user's choice (e.g., whether they installed the app or not)
+    console.log(choiceResult.outcome);
+    // Set the deferred prompt to null to prevent it from showing again
+    deferredPrompt = null;
+    // Hide the install button
+    butInstall.style.display = 'none';
+  }
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  // Log that the app has been installed
+  console.log('Jate has been installed.');
+});
